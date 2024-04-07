51af090523bb1cf95d0355042bc3599f3649842a
AdamNiesiobedzki <adam.niesiobedzki@o2.pl>
2024-04-07 16:05:11 +0200

    test-case-8

--- playground.ts
+++ playground.ts
@@ -3,59 +3,69 @@ import { z } from "./src";
 // Test case 1
 console.log("---CASE 1---");
 const s1 = z.string();
-const val1 = "ĄĆbńt"
+const val1 = "ĄĆbńt";
 console.log(s1.parse(val1));
 
 // Test case 2
 console.log("---CASE 2---");
 const s2 = z.string().uuid();
-const val2 = "550E8400-e29B-41d4-a71C-446A5544FF00"
+const val2 = "550E8400-e29B-41d4-a71C-446A5544FF00";
 console.log(s2.parse(val2));
 
 // Test case 3
 console.log("---CASE 3---");
 const s3 = z.number().finite();
 try {
-    s3.parse(undefined);
-}
-catch (error){
-    console.log("error");
+  s3.parse(undefined);
+} catch (error) {
+  console.log("error");
 }
 
 // Test case 4
 console.log("---CASE 4---");
-const s4 = z.enum(['foo', 'bar'], {
-    errorMap: () => ({message: "ERROR"})
-})
+const s4 = z.enum(["foo", "bar"], {
+  errorMap: () => ({ message: "ERROR" }),
+});
 try {
-    s4.extract(['foo']).parse('incorrect');
+  s4.extract(["foo"]).parse("incorrect");
+} catch (error) {
+  console.log(error);
 }
-catch (error) {
-    console.log(error);
-} 
 
 // Test case 5
 console.log("---CASE 5---");
 const s5 = z.object({
-    name: z.string(),
-    surname: z.string()
-})
+  name: z.string(),
+  surname: z.string(),
+});
 const tmp2 = s5.extend({
-    age: z.number()
-})
+  age: z.number(),
+});
 console.log(s5.shape);
 console.log(tmp2.keyof());
-
 
 // Test case 6
 console.log("---CASE 6---");
 const s6 = z.record(z.any());
-console.log(s6.parse({tmp3: undefined}));
+console.log(s6.parse({ tmp3: undefined }));
 
 // Test case 7
 console.log("---CASE 7---");
 const s7 = z.string().ip();
 console.log(s7.parse("ff06::3:0:c3"));
+
+//Test case 8 - enum test
+console.log("--CASE 8---");
+const schema_enum = ["Adam", "Tymek", "Łukasz"] as const;
+const test_enum = ["Adam", "Tymek", "Łukasz", "Mateusz"] as const;
+const s8 = z.enum(schema_enum);
+test_enum.forEach((element) => {
+  try {
+    console.log("succesfuly parsed element: " + s8.parse(element));
+  } catch {
+    console.log("error with parsed element: " + element);
+  }
+});
 
 // Test case 9
 console.log("---CASE 9---");
@@ -63,4 +73,8 @@ function tmp4() {}
 const s9 = z.string();
 console.log(s9.safeParse(tmp4));
 
-z;
+//Test case 10
+
+//Test case 11
+
+//Test case 12
