4133c4fbc3f06293c51e0085a1731c2c029c0197
AdamNiesiobedzki <adam.niesiobedzki@o2.pl>
2024-04-07 16:18:39 +0200

    updated-email-test

--- src/__tests__/jo-adam-white-box.test.ts
+++ src/__tests__/jo-adam-white-box.test.ts
@@ -3,6 +3,14 @@ import { expect, test } from "@jest/globals";
 
 import * as z from "../index";
 
+test("email without exclamation mark", () => {
+  const schema = z.string().email();
+
+  const emailWithoutExclamation = "test@mail.com";
+
+  expect(schema.safeParse(emailWithoutExclamation).success).toBe(true);
+});
+
 test("email with exclamation mark", () => {
   const schema = z.string().email();
 
